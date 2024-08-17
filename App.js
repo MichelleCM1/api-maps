be02ec02a455e54eaafe99a18c3d4996d528d6b3
MICHELLE CAROLINE MARTINS DOS SANTOS <michelle.cmsantos1@senacsp.edu.br>
2024-08-16 21:44:21 -0300

    add marker e tela escura

--- App.js
+++ App.js
@@ -1,13 +1,297 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Platform, PermissionsAndroid, Dimensions } from 'react-native';
+import MapView, {Marker, Callout} from "react-native-maps";
 
+import * as Location from "expo-location";
+
+const {width, height} = Dimensions.get('screen')
+const customMapStyle = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#181818"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1b1b1b"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#2c2c2c"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8a8a8a"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#373737"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3c3c3c"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#4e4e4e"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#3d3d3d"
+      }
+    ]
+  }
+];
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    const [region, setRegion] = useState(null)
+    const [markers, setMarkers] = useState([])
+    
+
+
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                console.log("Permissão negada");
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync({});
+            // console.log('LATITUDE: ', location.coords.latitude)
+            // console.log('LONGITUDE', location.coords.longitude)
+            setRegion({
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }
+            )
+
+        })();
+    }, []);
+
+    function newMarker(event) {
+        let dados = {
+            key: markers.length,
+            coords: {
+                latitude: event.nativeEvent.coordinate.latitude,
+                longitude: event.nativeEvent.coordinate.longitude,
+            },
+
+            pinColor: "red",
+            
+        }
+
+        setRegion({
+            latitude: event.nativeEvent.coordinate.latitude,
+            longitude: event.nativeEvent.coordinate.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+        })
+
+        setMarkers(oldArray => [...oldArray, dados])
+
+        markers.push(dados)
+        
+        
+        
+        
+    }
+
+    return (
+      <View style={styles.container}>
+        <MapView
+         style={styles.map}
+         region={region}
+         onMapReady={() => {
+             Platform.OS === "android" ?
+             PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+                 .then(() => {
+                     console.log("Permissão concedida");
+                 }) : ''
+         }}
+         onPress={event=> {
+             newMarker(event)
+         }}
+         zoomEnabled={true}
+         showsUserLocation={true}
+         loadingEnabled={true}
+         minZoomLevel={15}
+         customMapStyle={customMapStyle}
+        >
+          {markers.map(marker => {
+              return <Marker
+                  key={marker.key}
+                  coordinate={marker.coords}
+                  pinColor={marker.pinColor}
+
+              >
+                 { <Callout>
+              <View>
+                <Text>vc clicou aqui </Text>
+                <Text>Latitute:{marker.coords.latitude}</Text>
+                <Text>Longitude:{marker.coords.longitude}</Text>
+                <Text>Data: {new Date().toLocaleString()}</Text>
+               
+              </View>
+                </Callout> }
+
+              </Marker>
+          })}
+            {/* {<Marker coordinate={}></Marker>} */}
+           
+        </MapView>
+      </View>
   );
 }
 
@@ -18,4 +302,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  dimensao: {
+      width: width,
+      height: height,
+  }
 });
+
